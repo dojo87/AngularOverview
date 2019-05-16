e45e1a48d536280349fce0df6e9b7a34dcb86e7d
a305477 <wojciech.jamrozik@consultant.volvo.com>
2019-05-16 14:48:14 +0200

    3-Angular Material CDK

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule],
+      imports: [RouterTestingModule, FormsModule],
       declarations: [AppComponent]
     }).compileComponents();
   }));
@@ -22,12 +23,13 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('recourse');
   });
 
-  it('should render title in a h1 tag', () => {
+  it('should render title in text and input', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain(
-      'Welcome to recourse!'
-    );
+    fixture.whenStable().then(() => {
+      expect(compiled.textContent).toContain('recourse');
+      expect(compiled.querySelector('input').value).toContain('recourse');
+    });
   });
 });
